39f36c7a43f4557a5fe7c9132ecf0bfaf4389aa4
talgo <tolgabudak1990@gmail.com>
2025-01-27 11:36:49 +0300

    Ordering created

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container
+builder.Services
+    .AddApplicationService()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline
+app.UseApiServices();
+
+app.Run();
